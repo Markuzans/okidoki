14c14deef6de9321f7c2bb238c9986ec7f3cb1ae
Markuzans <119396400+Markuzans@users.noreply.github.com>
2023-03-28 09:35:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const music = document.querySelector('.js-music');
+const noo = document.querySelector('.js-noplz');
+
+music.addEventListener('click', ( ) => {window.open ('https://youtu.be/NRQRC_0ZQ00')})
+noo.addEventListener('click', ( ) => {window.open ('https://www.bing.com/ck/a?!&&p=57a865d960a036a4JmltdHM9MTY3OTg3NTIwMCZpZ3VpZD0zYzZiYzc0Zi0yMzgwLTZiZmEtMTZlNy1kNjc3Mjc4MDZkMzQmaW5zaWQ9NTU3OQ&ptn=3&hsh=3&fclid=3c6bc74f-2380-6bfa-16e7-d67727806d34&u=a1L2ltYWdlcy9zZWFyY2g_cT1DcnlpbmclMjBFbW9qaSZGT1JNPUlRRlJCQSZpZD02RDI0RkFGQzM5QUFCMTAxRTdBRTU1RUFCMTlBOEQzQjZDOTczODgx&ntb=1')})
+
